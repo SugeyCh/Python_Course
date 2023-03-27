ea0d93473aa43d7136277239c67a030a8edf729b
SugeyCh <chaconsugey2003@gmail.com>
2023-03-27 17:38:30 -0400

    Modifications

--- ejercicios/ejercicio-1.py
+++ ejercicios/ejercicio-1.py
@@ -21,6 +21,7 @@ diferencia_max  = 100 - este_curso * 1000 // otros_cursos_max / 10
 diferencia_prom = 100 - este_curso / otros_cursos_prom * 100
 
 # Mostrando las diferencias de duración (Parte A)
+print('--------------------------')
 print(f'Este curso dura un {diferencia_min}% menos que el más rápido')
 print(f'Este curso dura un {diferencia_max}% menos que el más lento')
 print(f'Este curso dura un {diferencia_prom}% menos que el promedio')
@@ -38,10 +39,14 @@ tiempo_vacio_promedio = 100 - otros_cursos_prom * 1000 // vacio_promedio / 10
 tiempo_vacio_curso    = 100 - este_curso * 1000 // vacio_curso / 10
 
 # Mostrando la cantidad de espacios vacíos que se remueven (Parte B)
+print('--------------------------')
 print(f'Un curso promedio elimina un {tiempo_vacio_promedio}% de tiempo vacío')
 print(f'Este curso eliminó el {tiempo_vacio_curso}% de tiempo vacío')
+print('--------------------------')
 
 
 # C - Ver 10 horas de este curso a cuantas de otros equivale? ¿y al revés?
 # Mostrando diferencias si los cursos duraran 10 horas (Parte C)
 print(f'Ver 10 horas de este curso equivale a ver {otros_cursos_prom * 100 // este_curso / 10} horas de otros cursos')
+print(f'Ver 10 horas de otros cursos equivale a ver {este_curso * 100 // otros_cursos_prom / 10} horas de este curso')
+print('--------------------------')
