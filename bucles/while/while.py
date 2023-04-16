823ac18060df4df972e4605c737fd4dc7950583f
SugeyCh <chaconsugey2003@gmail.com>
2023-04-15 21:42:37 -0400

    While Loop

---
+++ bucles/while/while.py
@@ -0,0 +1,9 @@
+# BUCLE WHILE
+# Permite repetir una sentencia varias veces siempre y cuando la condición se cumpla (sea True)
+contador = 0
+while contador < 10:
+  print(contador)
+  contador+=1
+  
+# Se pone: contador+=1, para que aumente su número, ya que si se deja solamente con el print, 
+# el código se seguirá ejecutando de manera infinita.
